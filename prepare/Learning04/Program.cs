fc731e6b4048b0b5a170c44c4d408a1827128572
Koigor97 <turaysamuel101@yahoo.com>
2023-02-13 22:53:22 +0200

    Complete the Inheritance Learning Activity.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine("Hello Learning04 World!\n");
+        Assignment assignment = new Assignment("Samuel Turay", "Programming");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine("");
+
+        MathAssignment mathAssignment = new MathAssignment("Sarah Alpha", "Fraction", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine("");
+
+        WritingAssignment writingAssignment = new WritingAssignment("Koirah", "Africa History", "The Period of West Africa Independence");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+        
     }
 }
